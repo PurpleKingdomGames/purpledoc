db01bf06dcbc768ad2b50c4a2482fac6d65d729c
davesmith00000 <davesmith.00000@gmail.com>
2024-10-18 09:39:33 +0100

    Upgraded os-lib

--- project.scala
+++ project.scala
@@ -1,6 +1,6 @@
 //> using scala "3.5.0"
 
-//> using dep com.lihaoyi::os-lib:0.11.1
+//> using dep com.lihaoyi::os-lib:0.11.2
 //> using dep com.lihaoyi::scalatags:0.13.1
 //> using dep com.lihaoyi::mainargs:0.7.6
 //> using dep org.virtuslab::scala-yaml:0.3.0
