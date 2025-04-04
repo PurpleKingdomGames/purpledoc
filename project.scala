41862cd08b1a41f5c504a80ee31d6ed9b93b885f
davesmith00000 <davesmith.00000@gmail.com>
2025-04-04 23:42:18 +0100

    Fixed #8: Updated OS-Lib to 0.11.4

--- project.scala
+++ project.scala
@@ -1,6 +1,6 @@
 //> using scala "3.6.4"
 
-//> using dep com.lihaoyi::os-lib:0.11.3
+//> using dep com.lihaoyi::os-lib:0.11.4
 //> using dep com.lihaoyi::scalatags:0.13.1
 //> using dep com.lihaoyi::mainargs:0.7.6
 //> using dep org.virtuslab::scala-yaml:0.3.0
