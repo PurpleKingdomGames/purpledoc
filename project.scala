470cf2bbc28f7c549329054a25ae4020933c131a
davesmith00000 <davesmith.00000@gmail.com>
2025-04-04 23:47:01 +0100

    Fixed #7: Update Laika to 1.3.2

--- project.scala
+++ project.scala
@@ -4,6 +4,6 @@
 //> using dep com.lihaoyi::scalatags:0.13.1
 //> using dep com.lihaoyi::mainargs:0.7.6
 //> using dep org.virtuslab::scala-yaml:0.3.0
-//> using dep org.typelevel::laika-io:1.2.0
+//> using dep org.typelevel::laika-io:1.3.2
 
 //> using test.dep org.scalameta::munit::1.0.2
